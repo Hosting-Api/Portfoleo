4ec7de22864acfdd8472b0c8a0a1c77a268f3257
Hosting-Api <131949339+Hosting-Api@users.noreply.github.com>
2023-05-04 12:11:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,9 +6,10 @@ export default function Home() {
     <div className="app">
       <Head>
         <title>Vinit Patel - Portfolio</title>
-    <meta name="google-site-verification" content="Fp6rZx9_R5lCi1zABXUh5pdevWK-T790siAH9KxF57I" />
-        <meta name="description" content="Vinit Patel's portfolio developed using Next.js." />
+    <meta name="description" content="Vinit Patel's portfolio developed using Next.js." />
         <meta name="keywords" content="Next.js" />
+    <meta name="google-site-verification" content="Fp6rZx9_R5lCi1zABXUh5pdevWK-T790siAH9KxF57I" />
+        
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Hero />
