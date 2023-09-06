e8e1563ab4b31dd0774a809fd8df7e20b58f22db
Hosting-Api <131949339+Hosting-Api@users.noreply.github.com>
2023-09-06 15:25:30 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://vinitservers.ml',
+    siteUrl: process.env.SITE_URL || 'https://vinitservers.me',
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
